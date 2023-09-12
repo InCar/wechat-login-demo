346c3ace0ee29118a26a262e281010b5ba1b5b68
XGH <ghxiong@incarcloud.com>
2023-09-12 14:40:25 +0800

    git version

--- web/Home.tsx
+++ web/Home.tsx
@@ -5,11 +5,13 @@ import { OnCode } from './OnCode';
 
 export const Home = () => {
     const [isOnCode, setIsOnCode] = useState(false);
+    const [ver, setVer] = useState<any>('');
 
     useEffect(() => {
         (async ()=>{
             const {data} = await axios.get('/api/version');
             console.log(data.version);
+            setVer(data);
         })();
 
         const path = window.location.pathname;
@@ -24,6 +26,9 @@ export const Home = () => {
                 <>
                     <h1>微信扫码登录示例</h1>
                     <Login />
+                    <p>
+                        当前版本：<a href={ver.sourceLink} target='_blank'>{ver.version}</a>
+                    </p>
                 </>
             }
         </>
