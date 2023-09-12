805d115d9d70f2d175121f146714c16bfcf151ea
XGH <ghxiong@incarcloud.com>
2023-09-12 15:51:56 +0800

    building time

--- web/Home.tsx
+++ web/Home.tsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { DateTime } from 'luxon';
 import { Login } from './Login';
 import { OnCode } from './OnCode';
 
 export const Home = () => {
     const [isOnCode, setIsOnCode] = useState(false);
     const [ver, setVer] = useState<any>('');
+    const [tmBuild, setTmBuild] = useState('');
 
     useEffect(() => {
         (async ()=>{
             const {data} = await axios.get('/api/version');
             console.log(data.version);
+            const tm = DateTime.fromMillis(data._tmBuilding);
             setVer(data);
+            setTmBuild(tm.setLocale("zh-CN").toLocaleString(DateTime.DATETIME_MED));
         })();
 
         const path = window.location.pathname;
@@ -28,6 +32,8 @@ export const Home = () => {
                     <Login />
                     <p>
                         当前版本：<a href={ver.sourceLink} target='_blank'>{ver.version}</a>
+                        <br />
+                        构建时间：{tmBuild}
                     </p>
                 </>
             }
